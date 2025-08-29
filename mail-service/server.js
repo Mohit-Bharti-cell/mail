49c828e4af64e4014b524f556ac359fbccfe0647
Mohit-Bharti-cell <mohit@netfotech.in>
2025-08-29 14:26:10 +0530

    Update server.js

--- mail-service/server.js
+++ mail-service/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'], // React dev servers
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://react-ai-frontend.vercel.app'], // React dev servers
   credentials: true
 }));
 
